11544aa222926b30fd949d018ebcd73a5fe67896
Devansh707 <devanshsrivastava707@gmail.com>
2021-09-24 13:31:07 +0530

    code written in editor

--- hk.js
+++ hk.js
@@ -2,7 +2,6 @@ const loginlink = "https://www.hackerrank.com/auth/login";
 const emailpassobj = require("./secrests");
 const {answers} = require("./codes");
 const puppeteer = require("puppeteer");
-
 let browserStackPromise = puppeteer.launch({
     headless: false,
     // slowMo : 1000,
@@ -43,12 +42,17 @@ browserStackPromise
         let waitfor3secondspromise = page.waitFor(3000);
         return waitfor3secondspromise;
     }).then(function (){
-        let challengeclickpromise = page.click(".challenge-submit-btn", {delay:100});
-        return challengeclickpromise;
+        let allChalengesArrPromise = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled", {delay:100});
+        console.log("problem reached");
+        return allChalengesArrPromise;
+    }).then(function (questionsArr){
+        console.log("number of questions", questionsArr.length);
+        let qWillbesolvedPromise = questionSolver(page, questionsArr[0], answers[0]);
+        return qWillbesolvedPromise;
+
+    }).then(function (){
+        console.log("question is solved");
     })
-
-
-
     function waitAndClick(selector, cPage) {
         return new Promise(function (resolve, reject) {
             let waitForModalPromise = cPage.waitForSelector(selector, { visible: true });
@@ -64,6 +68,37 @@ browserStackPromise
                 })
         }
         )
+    }
+
+    function questionSolver(page, questionnumber, answer){
+        return new Promise(function (resolve, reject) {
+            let qWillbeclickedPromise = questionnumber.click();
+            // code question
+            // in hk editor-> ctrl + a, ctrl + x
+            qWillbeclickedPromise.then(function (){
+                let waitForEdotortobeFocusedPromise = waitAndClick(".monaco-editor.no-user-select.vs", page);
+                return waitForEdotortobeFocusedPromise;
+            }).then(function () {
+                let ctrlPressedPromise = page.keyboard.down("Control");
+                return ctrlPressedPromise;
+            }).then(function () {
+                let ctrlApressedPromise = page.keyboard.press("A", {delay:1500});
+                return ctrlApressedPromise;
+            }).then(function () {
+                console.log("Ctrl + A, Ctrl + X Pressed");
+                return page.keyboard.press("X", {delay : 500});
+            }).then(function (){
+                return page.keyboard.up("Control");
+            }).then(function (){
+                // fails because of autocomplete feature of monaco editor
+                return page.keyboard.type(answer, {delay : 50});
+            }).then(function(){
+                resolve();
+            }).catch(function (err) {
+                console.log(err);
+                reject(err);
+            })
+        })
     }
 
 
